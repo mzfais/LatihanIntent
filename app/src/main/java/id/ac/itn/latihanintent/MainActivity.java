6fc7e77bf88e8bb09c6a02db45e137019e4fa2cd
Ahmad Faisol <mzfais@gmail.com>
2019-09-22 17:16:48 +0700

    Add 2 Activity

---
+++ app/src/main/java/id/ac/itn/latihanintent/MainActivity.java
@@ -0,0 +1,23 @@
+package id.ac.itn.latihanintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnPindah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
